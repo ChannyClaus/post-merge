e21c386a77e366e820aa8292b5b97bdfdc192de7
Chan Kang <chankang17@gmail.com>
2024-10-07 23:59:54 -0400

    allow overriding sqlalchemy.url via DB_URL environment variable

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -39,6 +40,9 @@ def run_migrations_offline() -> None:
 
     """
     url = config.get_main_option("sqlalchemy.url")
+
+    if "DB_URL" in os.environ:
+        url = os.environ["DB_URL"]
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -64,9 +68,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
