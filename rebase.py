7b55e3ec26f8790d40c01e931b24f3f776ea209b
Chan Kang <chankang17@gmail.com>
2024-10-10 03:06:42 -0400

    seems functional

--- rebase.py
+++ rebase.py
@@ -1,10 +1,11 @@
 import argparse
+from contextlib import redirect_stdout
 import io
 from alembic.config import Config
 from alembic import command
 
 
-def main(parent):
+def main(new_parent):
     alembic_cfg = Config("alembic.ini")
 
     # alembic doesn't support returning a structured
@@ -15,16 +16,38 @@ def main(parent):
     command.branches(alembic_cfg, "head")
 
     stdout_buffer.seek(0)
-    head_revisions = [
-        line.replace("             -> ", "")[:12]
-        for line in stdout_buffer.read().split("\n")
-        if "(head)" in line
-    ]
+    head_revisions = set(
+        [
+            line.replace("             -> ", "")[:12]
+            for line in stdout_buffer.read().split("\n")
+            if "(head)" in line
+        ]
+    )
 
-    if len(head_revisions) > 2:
+    if len(head_revisions) != 2:
         raise Exception(
             f"Found {len(head_revisions)} head revisions ({head_revisions}), expected 2"
         )
+
+    if new_parent not in head_revisions:
+        raise Exception(
+            f"provided new parent revision {new_parent} not found in head revisions ({head_revisions})"
+        )
+
+    child = (head_revisions - set([new_parent])).pop()
+
+    stdout_buffer.seek(0)
+    command.show(alembic_cfg, child)
+    stdout_buffer.seek(0)
+
+    lines = [line for line in stdout_buffer.read().split("\n")]
+    parent_to_replace = lines[1].replace("Parent: ", "")
+    path = lines[2].replace("Path: ", "")
+
+    new_file_content = open(path).read().replace(parent_to_replace, new_parent)
+
+    with open(path, "w") as f:
+        f.write(new_file_content)
 
 
 if __name__ == "__main__":
