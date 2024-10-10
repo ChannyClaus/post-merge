d181b2f917a44045171e3837239d08766f0db755
ChannyClaus <ChannyClaus@users.noreply.github.com>
2024-10-10 07:19:48 +0000

    rebasing the migration with the new parent revision "ea949c1f8747"

--- alembic/versions/1960af0aaf3d_.py
+++ alembic/versions/1960af0aaf3d_.py
@@ -1,7 +1,7 @@
 """empty message
 
 Revision ID: 1960af0aaf3d
-Revises: e14bc49bed87
+Revises: ea949c1f8747
 Create Date: 2024-10-10 01:45:25.861437
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '1960af0aaf3d'
-down_revision: Union[str, None] = 'e14bc49bed87'
+down_revision: Union[str, None] = 'ea949c1f8747'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
